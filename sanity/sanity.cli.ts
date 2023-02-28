c2070634af0fa8e189b750ab90af5092a7a1cd38
HyungJinHan <han1210_36@naver.com>
2023-03-01 05:17:40 +0900

    .

--- sanity/sanity.cli.ts
+++ sanity/sanity.cli.ts
@@ -3,6 +3,6 @@ import {defineCliConfig} from 'sanity/cli'
 export default defineCliConfig({
   api: {
     projectId: 'jxxv2ra8',
-    dataset: 'production'
-  }
+    dataset: 'production',
+  },
 })
