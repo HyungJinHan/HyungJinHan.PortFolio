67e3027082ff52fd0de76d96a74e222998c5e148
HyungJinHan <han1210_36@naver.com>
2023-03-01 06:13:21 +0900

    .

--- sanity.ts
+++ sanity.ts
@@ -2,8 +2,8 @@ import { createClient } from "next-sanity";
 import creatImageUrlBuilder from "@sanity/image-url";
 
 const config = {
-  dataset: process.env.NEXT_PUBLIC_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_PROJECT_ID!,
+  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
+  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
   apiVersion: "2021-10-21",
   useCdn: process.env.NODE_ENV === "production",
 };
