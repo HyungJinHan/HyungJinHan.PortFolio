74385ddf7da603745830561e381321ba7e83303d
HyungJinHan <han1210_36@naver.com>
2023-03-01 17:22:04 +0900

    .

--- sanity.config.ts
+++ sanity.config.ts
@@ -3,7 +3,7 @@ import creatImageUrlBuilder from "@sanity/image-url";
 
 const config = {
   dataset: process.env.NEXT_PUBLIC_SANITY_DATASET || "production",
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID!,
+  projectId: "jxxv2ra8",
   apiVersion: "2022-11-15",
   useCdn: process.env.NODE_ENV === "production",
 };
